10e23b7c2669119f50c22d2fdccb83fb264e8483
suyingliang727-blip <suyingliang727@gmail.com>
2026-07-16 11:28:00 +0800

    Add files via upload
    修复3

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const SITE_VERSION = 'v35';
-const CACHE_NAME = 'her-lens-v35';
-const STATIC_CACHE = 'her-lens-static-v35';
-const IMAGE_CACHE = 'her-lens-images-v35';
+const SITE_VERSION = 'v37';
+const CACHE_NAME = 'her-lens-v37';
+const STATIC_CACHE = 'her-lens-static-v37';
+const IMAGE_CACHE = 'her-lens-images-v37';
 
 const STATIC_ASSETS = [
     './',
