c3499cb78b4daa9d9d3cbf53f0900f1eb11523b3
suyingliang727-blip <suyingliang727@gmail.com>
2026-07-19 06:36:28 +0800

    Add files via upload
    引导优化

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const SITE_VERSION = 'v60';
-const CACHE_NAME = 'her-lens-v60';
-const STATIC_CACHE = 'her-lens-static-v60';
-const IMAGE_CACHE = 'her-lens-images-v60';
+const SITE_VERSION = 'v61';
+const CACHE_NAME = 'her-lens-v61';
+const STATIC_CACHE = 'her-lens-static-v61';
+const IMAGE_CACHE = 'her-lens-images-v61';
 
 const STATIC_ASSETS = [
     './',
